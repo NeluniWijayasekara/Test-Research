22f987924ad40df3076f9337eaa31d492b049534
MATHEESHA <95665347+MATHEESHA2@users.noreply.github.com>
2025-05-14 14:06:14 +0530

    `Create label_map.py`

---
+++ utils/label_map.py
@@ -0,0 +1,18 @@
+# Dictionary mapping English symptom names to their Sinhala translations
+label_map = {
+    "headache": "මගේ හිස වේදනාව",
+    "fever": "මට උණක් තියෙනවා",
+    "cough": "මට කැස්සක් තියෙනවා",
+    "cold": "මට සෙරිනවා",
+    "chest_pain": "මට නැවතීමක් තියෙනවා",
+    "vomiting": "මට වමනයක් එනවා",
+    "diarrhea": "මට පිටවීමක් තියෙනවා",
+    "back_pain": "මට පිට වේදනාවක් තියෙනවා",
+    "joint_pain": "මට සංධි වේදනාවක් තියෙනවා",
+    "stomachache": "මට බඩ කැක්කුමක් තියෙනවා",
+    "dizziness": "මට හිසරදයක් තියෙනවා",
+    "tiredness": "මට පසුබැසීමක් හෝ මහන්සියක් ඇති",
+    "sore_throat": "මට ගිලන්බෙර වේදනාවක් තියෙනවා",
+    "toothache": "මට දත් වේදනාවක් තියෙනවා",
+    "eye_pain": "මට ඇස් වේදනාවක් තියෙනවා"
+}
